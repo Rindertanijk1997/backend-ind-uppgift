cf6a1d8282189b4ce405b8ef9de148c90f04fe63
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-06-09 17:00:33 +0200

    2 klar

--- routes/products.js
+++ routes/products.js
@@ -23,4 +23,44 @@ router.post('/', loadMenu, validateNewProduct, (req, res) => {
     res.status(201).json(newProduct);
 });
 
+router.put('/:id', loadMenu, (req, res) => {
+    console.log(req.menu);  // Lägg till detta för att se vad som loggas ut
+    const { id } = req.params;
+    const { title, desc, price } = req.body;
+    const product = req.menu.find(p => p.id === id);
+
+
+    if (!product) {
+        return res.status(404).json({ message: "Produkten hittades inte" });
+    }
+
+    let isModified = false;
+
+    // Kontrollera och uppdatera titeln
+    if (title && title !== product.title) {
+        product.title = title;
+        isModified = true;
+    }
+
+    // Kontrollera och uppdatera beskrivningen
+    if (desc && desc !== product.desc) {
+        product.desc = desc;
+        isModified = true;
+    }
+
+    // Kontrollera och uppdatera priset
+    if (price && price !== product.price) {
+        product.price = price;
+        isModified = true;
+    }
+
+    // Lägg till modifiedAt om någon egenskap har ändrats
+    if (isModified) {
+        product.modifiedAt = new Date().toISOString();
+    }
+
+    // Skicka tillbaka den uppdaterade produkten som respons
+    res.json(product);
+});
+
 export default router;
